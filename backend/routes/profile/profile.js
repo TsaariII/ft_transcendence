4bb3d811d9776e0ce25a6db1bf9f07fd9c122226
¨Nikolai <nzharkev@student.hive.fi>
2025-12-02 18:30:03 +0200

    Saving

--- backend/routes/profile/profile.js
+++ backend/routes/profile/profile.js
@@ -228,15 +228,15 @@ async function profileRoutes(fastify, options)
 			return reply.code(400).send({error: 'Invalid target'});
 		try
 		{
-			const profile = await DBget.fetchUser(user_id);
+			const profile = await DBget.fetchUser(viewerId);
 			if (!profile)
 				return reply.code(404).send({error: 'User not found'});
-			const matchHistory = await DBget.getMatchHistory(user_id);
+			const matchHistory = await DBget.getMatchHistory(viewerId);
 			const tournamentWins = await new Promise((resolve, reject) => {
 				db.get(
 					`SELECT COUNT(*) AS wins
 						FROM tournaments
-					WHERE winner_id = ?`, [user_id],
+					WHERE winner_id = ?`, [viewerId],
 					(err, row)  => {
 						if (err) return reject(err);
 						resolve(row?.wins ?? 0);
